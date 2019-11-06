86c4d3b45fda96f4756e720fe51f0c9799db331f
Sarah Jane Mojica <mojicasarahjane@gmail.com>
2019-11-07 00:56:40 +0800

    fixed ui. added react hooks to fetch api

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,26 +1,155 @@
-import React from 'react';
-import logo from './../logo.svg';
+import React , { useState, useEffect } from 'react';
 import './styles/App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Avatar from '@material-ui/core/Avatar';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import { Business, Videocam, ImportantDevices } from '@material-ui/icons';
+import TableComponent from './TableComponent';
+import Fade from '@material-ui/core/Fade';
+// import Grid from '@material-ui/core/Grid';
+import Snackbar from '@material-ui/core/Snackbar';
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex'
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  toolbar: theme.mixins.toolbar,
+}));
+
 
 const App: React.FC = () => {
+  const classes = useStyles();
+  const [page, setPage] = useState(1);
+  const [category, setCategory] = useState('');
+  const [data, setData] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [open, setOpen] = React.useState(false);
+
+  const pageSize = 20;
+
+  const fetchMore = (category : string) => {
+    setPage(page + 1);
+    setCategory(category);
+  };
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch(
+      `http://localhost:3000/api/news/sources?category=${category}&page=${page}&pageSize=${pageSize}`,
+      {
+        method: "GET",
+        headers: new Headers({
+          Accept: "application/json"
+        })
+      }
+    )
+      .then(res => res.json())
+      .then(response => {
+        setError(response.error);
+        setData(response.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        setError(error);
+        setLoading(false)
+      });
+  }, [category, page]);
+
+  function handleClose() {
+    setOpen(false);
+  }
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  
+        <div className={classes.root}>
+
+        <CssBaseline />
+        <AppBar position="fixed" className={classes.appBar}>
+          <Toolbar />
+        </AppBar>
+        
+        <Drawer
+          className={classes.drawer}
+          variant="permanent"
+          classes={{
+            paper: classes.drawerPaper,
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <div className={classes.toolbar} />
+            <List>
+              <ListItem button key={"business"}>
+                <ListItemAvatar>
+                  <Avatar>
+                    <Business />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary="Business" secondary="News Source 1" onClick={() => fetchMore('business')}/>
+              </ListItem>
+              <Divider />
+              <ListItem  button key={"technology"}>
+                <ListItemAvatar>
+                  <Avatar>
+                    <ImportantDevices />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary="Technology" secondary="News Source 2" onClick={() => fetchMore('technology')}/>
+              </ListItem>
+              <Divider />
+              <ListItem button key={"entertainment"}>
+                <ListItemAvatar>
+                  <Avatar>
+                    <Videocam />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary="Entertainment" secondary="News Source 3" onClick={(e) => fetchMore('entertainment')}/>
+              </ListItem>
+              <Divider />
+          </List>
+        </Drawer>
+        { error ? 
+        <Snackbar
+          open={true}
+          onClose={handleClose}
+          autoHideDuration={3000}
+          TransitionComponent={Fade}
+          message={<span id="message-id">Something went wrong....</span>}
+        />: 
+            data && data.length > 1 ? <TableComponent data={data}/> : error ? <div> error ... </div> : null
+        }
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+        </main>
+      </div>
     </div>
-  );
+  )
+  
 }
 
 export default App;
---
+++ src/components/TableComponent.tsx
@@ -0,0 +1,109 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+import Container from '@material-ui/core/Container';
+
+const columns = [
+  { id: 'name', label: 'Article Title', minWidth: 170 },
+  { id: 'description', label: 'Description', minWidth: 170 },
+  { id: 'date', label: 'Date', minWidth: 170 },
+  { id: 'image', label: 'Image', minWidth: 100 }
+];
+
+function createData(name: string, date: string, image : string, description : string) {
+  return { name, date, image, description };
+}
+
+const rows = [
+  createData('India', '2019-10-11', 'image1', 'Test desc'),
+  createData('China', '2019-10-11', 'image1', 'Test desc'),
+  createData('Italy', '2019-10-11', 'image1', 'Test desc'),
+  createData('United States', '2019-10-11', 'image1', 'Test desc')
+];
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+    margin: '2%',
+    position: 'sticky'
+  },
+  tableWrapper: {
+    maxHeight: 440,
+    overflow: 'auto',
+  },
+});
+
+export default function TableComponent(value : any ) {
+  const { data } = value;
+  const classes = useStyles();
+  const [page, setPage] = useState(0);
+
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const handleChangePage = (event : any , newPage : any) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event : any ) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+ 
+  return (
+    <Paper className={classes.root}>
+      <div className={classes.tableWrapper}>
+        <Table stickyHeader aria-label="sticky table" size="medium">
+          <TableHead>
+            <TableRow>
+              {columns.map((column: any) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data && data.length > 1 ? data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row : any ) => {
+              return (
+                <TableRow key={row.id}>
+                  {columns.map((column : any ) => {
+                    const value = row[column.id] ? row[column.id] : '';
+                    return (
+                      <TableCell key={column.id} align={column.align}>
+                        {column.format && typeof value === 'number' ? column.format(value) : value}
+                      </TableCell>
+                    );
+                  })}
+                </TableRow>
+              );
+            }) : null}
+          </TableBody>
+        </Table>
+      </div>
+      <TablePagination
+        rowsPerPageOptions={[20, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        backIconButtonProps={{
+          'aria-label': 'previous page',
+        }}
+        nextIconButtonProps={{
+          'aria-label': 'next page',
+        }}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
